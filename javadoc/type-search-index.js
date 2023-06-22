8dd696cbf9c5ffe3dbd3b63985e2ca3bfc4c7774
Oleh Babych <136449296+olehbabych3@users.noreply.github.com>
2023-06-22 06:10:18 +0200

    initial commit documentation commit

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Book"},{"p":"model","l":"Library"},{"p":"app","l":"LibraryApp"}];updateSearchResults();
