713f8e39eaffa705cf922ab4397effbc66a6cddd
Oleh Babych <136449296+olehbabych3@users.noreply.github.com>
2023-06-22 06:02:06 +0200

    initial commit

--- src/main/java/app/LibraryApp.java
+++ src/main/java/app/LibraryApp.java
@@ -7,14 +7,30 @@ import model.Library;
  * Aplikacja testująca funkcjonalność biblioteki.
  */
 public class LibraryApp {
+
+    /**
+     * Domyślny konstruktor klasy LibraryApp.
+     * Tworzy nową instancję LibraryApp.
+     */
+    public LibraryApp() {
+        // Domyślny konstruktor
+    }
+
+    /**
+     * Metoda główna aplikacji.
+     *
+     * @param args argumenty wiersza poleceń
+     */
     public static void main(String[] args) {
         // Tworzenie biblioteki
         Library library = new Library();
+
         // Dodawanie książek
         Book book1 = new Book("Wiedźmin", "Andrzej Sapkowski");
         Book book2 = new Book("Harry Potter", "J.K. Rowling");
         library.addBook(book1);
         library.addBook(book2);
+
         // Wyświetlanie książek w bibliotece
         System.out.println("Książki w bibliotece:");
         for (Book book : library.getBooks()) {
